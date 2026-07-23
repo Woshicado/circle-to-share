aeb956819d5655e2bde0fe2e1d30dd4ad670eb23
Joshua Meyer <woshicado@woshicado.eu>
2026-07-23 18:21:23 +0200

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "eu.woshicado.circletoshare"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
     }
 
     signingConfigs {
